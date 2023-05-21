4e43fe3fc181fb7f2c7d9983ace737947650bd8f
Pavel <mrpancho@mail.ru>
2023-05-21 19:55:49 +0300

    practic part3

--- test/script.js
+++ test/script.js
@@ -1,13 +1,6 @@
 'use strict';
 
-
 let numberOfFilms;
-/* star: for ( let j = 0; j < 10 ; j++ ) {
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-if (numberOfFilms === null || numberOfFilms === '' /* || numberOfFilms.length >=5 */  /* ) {
-    continue star;
-} else break;
-} */
 
 function start()  {
   numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
@@ -26,6 +19,8 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+
+// ------------------------------------------------------------------
 /* const a = prompt('Один из последних просмотренных фильмоы?', ''),
       b = prompt('На сколько оцените его?',''),
       c = prompt('Один из последних просмотренных фильмоы?', ''),
@@ -34,9 +29,24 @@ const personalMovieDB = {
 /* personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d; */
 
-
+// -----------------------------------------------------------------
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    console.log(i);
+    const a = prompt('Один из последних просмотренных фильмоы?', ''),
+          b = prompt('На сколько оцените его?','');
+          if ( a !=null && b !=null && a !='' && b !='' && a.length < 50 ) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+          } else {
+            console.log('error');
+            i--;
+          }
+} 
+}
+rememberMyFilms();
+//-----------------------------------------------------------------
 // -------------------НАПИСАНО ЦИЛОМ FOR -------------------
-
 /*  for (let i = 0; i < 2; i++) {
     console.log(i);
     const a = prompt('Один из последних просмотренных фильмоы?', ''),
@@ -48,7 +58,7 @@ personalMovieDB.movies[c] = d; */
             console.log('error');
             i--;
           }
-}  */   
+}    */
 
 // -------------------НАПИСАНО ЦИЛОМ DO WHILE ------------------- 
 /* let i = 0;
@@ -68,7 +78,7 @@ do {
 while ( i < 2 );  */ 
             
  // -------------------НАПИСАНО ЦИЛОМ WHILE -------------------    
-let i = 0;
+/* let i = 0;
 while ( i < 2 ) {
     console.log(i);
     const a = prompt('Один из последних просмотренных фильмоы?', ''),
@@ -81,10 +91,10 @@ while ( i < 2 ) {
             console.log('error');
             i--;
         }
-    }
+    } */
 
-
-if (personalMovieDB.count < 10 ) {
+// ------------------Условия----------------------------------------
+/* if (personalMovieDB.count < 10 ) {
         console.log("просмотрено довольно мало филтмов");
 } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
 console.log("Вы классический зритель");
@@ -92,6 +102,37 @@ console.log("Вы классический зритель");
     console.log("Вы кономан")
 } else {
     console.log("Произошла ошибка");
-} 
+}  */
+//------------------------------------------------------------------
 
-console.log(personalMovieDB);
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10 ) {
+    console.log("просмотрено довольно мало филтмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
+console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30 ){
+console.log("Вы кономан")
+} else {
+console.log("Произошла ошибка");
+}
+}
+
+detectPersonalLevel();
+
+function showMyDB (hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB);
+  }
+}
+
+showMyDB(personalMovieDB.privat);
+
+
+function writeYourGenres () {
+  for (let i = 1 ; i < 4 ; i++) {
+    personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i} `);
+  }
+}
+
+writeYourGenres();
+//console.log(personalMovieDB);
