84d1108557a8b4bd9730ad580d96093059d20ad0
Pavel <mrpancho@mail.ru>
2023-05-30 01:20:26 +0300

    project44

--- 44. project-mDB/project/js/script.js
+++ 44. project-mDB/project/js/script.js
@@ -23,4 +23,42 @@ const movieDB = {
         "Скотт Пилигрим против..."
     ]
 };
+//1 Удалить все рекламные блоки со страницы (правая часть сайта)
+const promoimg = document.getElementsByTagName('img');
+console.log(promoimg);
+
+promoimg[1].remove();
+promoimg[2].remove();
+promoimg[1].remove();
+
+console.log(promoimg);
+
+const promoss = document.getElementsByClassName('promo__adv');
+promoss[0].remove();
+
+//2 Изменить жанр фильма, поменять "комедия" на "драма"
+const genre = document.querySelector('.promo__genre');
+genre.innerHTML = "Драма";
+
+//3 Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img. Реализовать только при помощи JS
+const bg = document.querySelector('.promo__bg');
+// bg.style.background = "../img/bg.jpg";
+/* function bgg() {
+    bg.style.backgroundImage = 'url("img/bg.jpg")';
+  }
+  function bgg2() {
+    bg.style.backgroundImage = 'url("img/mars.webp")';
+  }
+  
+  setTimeout(bgg, 1000);
+  setTimeout(bgg2, 2000); */
+  
+  let timerId = setTimeout(function tick() {
+    bg.style.backgroundImage = 'url("img/bg.jpg")';
+    timerId = setTimeout(tick, 2000); 
+  }, 2000);
+  let timerId2 = setTimeout(function tick2() {
+    bg.style.backgroundImage = 'url("img/mars.webp")';
+    timerId2 = setTimeout(tick2, 2000); 
+  }, 3000);
 
