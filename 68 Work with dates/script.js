c03927a974ca4857051d01eb0edc4d0ec90b212b
Pavel <mrpancho@mail.ru>
2023-06-22 21:38:03 +0300

    Home work

--- 68 Work with dates/script.js
+++ 68 Work with dates/script.js
@@ -1,2 +1,42 @@
 'use strict';
 
+// const deadline = '2023-06-22 GMT+03:00'
+const now = new Date();
+alert(now);
+// console.log(Math.floor((Date.parse('2023-06-22') - Date.parse(new Date())) / (1000 * 60 * 60)% 24));
+
+/* console.log(now);
+
+
+var d = new Date('August 19, 1975 23:15:30');
+console.log(d.getTimezoneOffset());
+
+
+
+var d = new Date();
+d.setTime(d.getTime()+ 60*180*1000);
+console.log(d);
+ */
+/*
+console.log(now.getFullYear());
+console.log(now.getMonth());
+console.log(now.getDate());
+console.log(now.getDay());
+console.log(now.getHours());
+console.log(now.getMinutes());
+console.log(now.getUTCHours());
+
+console.log(now.getTimezoneOffset());
+console.log(now.getTime()); */
+/* console.log(now);
+console.log(now.setHours(20)); */
+// console.log(now); 
+
+
+/* let start = new Date();
+for( let i = 0; i < 10000000; i++) {
+    let some = i**3;
+}
+let end = new Date();
+
+console.log((`Цикл отработал за ${end - start} миллисекунд`)); */
